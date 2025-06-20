3144f498d877217eef938153f73e5ee9cab52128
holden <james.holden.moore@gmail.com>
2025-06-19 20:58:30 -0400

    Error Handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,6 @@
 const User = require("../models/user");
 
-// GET /users
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((err) => {
-      console.error(err);
-      return res.status(500).send({ message: err.message });
-    });
-};
-
+// POST /users
 const createUser = (req, res) => {
   const { name, avatar } = req.body;
 
@@ -19,7 +10,18 @@ const createUser = (req, res) => {
       console.error(err);
       if (err.name === "ValidationError") {
         return res.status(400).send({ message: err.message });
+      } else {
+        return res.status(500).send({ message: err.message });
       }
+    });
+};
+
+// GET /users
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      console.error(err);
       return res.status(500).send({ message: err.message });
     });
 };
