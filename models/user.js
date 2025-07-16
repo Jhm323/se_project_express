d310aa02df75fda37976f250ab2ed1d9a18c30cd
holden <james.holden.moore@gmail.com>
2025-07-16 16:53:59 -0400

    New userSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const validator = require("validator");
+
+const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
@@ -30,9 +33,28 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
   },
 });
+
 userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
-) {};
+) {
+  // trying to find the user by email
+  return this.findOne({ email }) // this — the User model
+    .then((user) => {
+      // not found - rejecting the promise
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      // found - comparing hashes
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+
+        return user; // now user is available
+      });
+    });
+};
 
 module.exports = mongoose.model("user", userSchema);
