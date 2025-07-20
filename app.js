31099e26a28131001782ea7ad2e1d4e580e0448e
holden <james.holden.moore@gmail.com>
2025-07-19 23:28:58 -0400

    express-rate-limit & Helmet middleware

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -17,7 +27,11 @@ mongoose
 
 app.use(express.json());
 app.use(cors());
+app.use(limiter);
+app.use(helmet());
 app.use(mainRouter);
+
+app.disable("x-powered-by");
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
